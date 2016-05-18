d557c92fab6270b8ece7c89ad7cbd79cc700d685
Ernesto Paz <Ernesto-Paz@users.noreply.github.com>
2016-05-18 16:08:34 -0400

    Added basic messaging events and event handling for both server and client.

--- server.js
+++ server.js
@@ -1,10 +1,43 @@
 var PORT = process.env.PORT || 3000;
 var express = require("express");
+var path = require('path');
 var app = express();
-var http = require("http").Server(app);
+var server = require("http").createServer(app); // creates server 
+var io = require("socket.io").listen(server); // listens to server for requests.
+
+app.locals.pretty = true;
 
 app.use(express.static(__dirname + "/public"));
 
-http.listen(PORT, function (){
-console.log("Server started.")
+io.on("connection", function (socket) {
+
+    console.log("SOCKET.IO: ", "User connected.");
+
+    socket.on("message", function (message) {
+        socket.broadcast.emit("message", {
+            text: message.text
+        });
+        console.log("Server recieved: " + message.text)
+    });
+
+    socket.emit("message", {
+        text: "Welcome to the chat application."
+    })
+
+
+})
+app.set('views', __dirname + '/views');
+
+app.set('view engine', 'jade');
+
+server.listen(PORT, function () {
+    console.log("Server started.")
+});
+
+app.get("/", function (req, res) {
+    res.render("index", {
+        info: {
+            teststuff: "A test string that got inputted by the server dynamically!"
+        }
+    });
 });
