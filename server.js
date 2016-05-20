d9091f551e5477505040a669e3c908b3cff54a4a
Ernesto Paz <Ernesto-Paz@users.noreply.github.com>
2016-05-20 14:35:28 -0400

    Added connect and disconnection messages.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var path = require('path');
 var app = express();
 var server = require("http").createServer(app); // creates server 
-var io = require("socket.io").listen(server);// listens to server for requests.
+var io = require("socket.io").listen(server); // listens to server for requests.
 var moment = require("moment");
 
 app.locals.pretty = true;
@@ -20,20 +20,28 @@ io.on("connection", function (socket) {
 
     socket.on("message", function (message) {
         io.emit("message", {
-            time:moment().valueOf("X"),
+            time: moment().utc().valueOf("X"), //outgoing message does have timestamp
             text: message.text
         });
-        console.log("Server recieved: " + message.text)
-        console.log(message);
+        console.log(message); //message from client does not have timestamp;
     });
 
-    socket.emit("message", {
+    io.emit("message", {
         time: moment().valueOf("X"),
-        text: "Welcome to the chat application."
+        text: "A user has connected."
     })
 
+    socket.on("disconnect", function (socket) {
+        io.emit("message", {
+            time: moment().valueOf("X"),
+            text: "A user has disconnected."
+        })
 
-})
+
+    })
+
+});
+
 
 server.listen(PORT, function () {
     console.log("Server started.")
